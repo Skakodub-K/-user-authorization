fd9fe92c18444e95a5c1cbbef60bd017d460c9e5
Skakodub-K <skakodub03@bk.ru>
2024-11-28 22:27:36 +0300

    fixing checkSignature and change type number to bigint

--- server/generateSignature.js
+++ server/generateSignature.js
@@ -1,34 +1,34 @@
 // Импортируем функции
 const {
-    modularExponentiation,
-    gcd,
-    extendedGCD
-} = require('./calculations.js'); 
+  modularExponentiation,
+  gcd,
+  extendedGCD,
+} = require("./calculations.js");
 
 function generateSignature(openKey, base, mod, privateKey, hash) {
-    // Преобразуем строку в число (BigInt)
-    const bigIntNumber = BigInt("0x" + hash);
+  // Преобразуем строку в число (BigInt)
+  const bigIntNumber = BigInt("0x" + hash);
 
-    const o = Number(openKey);
-    const c = Number(privateKey);
-    
-    // Текст
-    const m = Number(bigIntNumber) % mod;
-    
-    // Случайное число 
-    let k;
-    do {
-        // Генерируем случайное число в диапазоне от 0 до mod
-        k = Math.floor(Math.random() * (mod - 1));
-    } while (gcd(k, mod - 1) !== 1); // Проверяем, что НОД равен 1
+  const o = BigInt(openKey); // Преобразуем openKey в BigInt
+  const c = BigInt(privateKey); // Преобразуем privateKey в BigInt
 
-    let r = modularExponentiation(base, k, mod);//'base ^ k';
-    const gcdXY = extendedGCD(k, mod - 1);
-    const rK = gcdXY.x;//k ^ -1
-    let s = rK * (m - c * r); //'k ^ -1 * (m - privateKey * r) mod (p-1)'
-    s = s % (mod - 1);
-    s = s > 0 ? s : s + (mod - 1); 
-    return [r, s];
+  // Текст
+  const m = bigIntNumber % BigInt(mod); // Используем BigInt для модульного выражения
+
+  // Случайное число
+  let k;
+  do {
+    // Генерируем случайное число в диапазоне от 0 до mod
+    k = BigInt(Math.floor(Math.random() * (mod - 1))); // Преобразуем в BigInt
+  } while (gcd(k, BigInt(mod) - BigInt(1)) !== BigInt(1)); // Проверяем, что НОД равен 1
+
+  let r = modularExponentiation(BigInt(base), k, BigInt(mod)); // 'base ^ k'
+  const gcdXY = extendedGCD(k, BigInt(mod) - BigInt(1));
+  const rK = gcdXY.x; // k ^ -1
+  let s = (rK * (m - c * r)) % (BigInt(mod) - BigInt(1)); // 'k ^ -1 * (m - privateKey * r) mod (p-1)'
+  s = s > BigInt(0) ? s : s + (BigInt(mod) - BigInt(1));
+
+  return [r.toString(), s.toString()];
 }
 
 module.exports = generateSignature; // Экспортируем только handler
